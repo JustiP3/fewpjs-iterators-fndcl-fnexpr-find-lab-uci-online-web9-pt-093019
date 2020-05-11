d27bb1fa92ba695363bb80cc59f8f52b7f1280b2
Justin <justinphil02@yahoo.com>
2020-05-10 21:16:34 -0700

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array) {
+  // {year: 1234, result: "W" || "L"}
+
+  const win = array.find(function(element) {
+    element.result === "W"
+  })
+  return win.year  
+}
